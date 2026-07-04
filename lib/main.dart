74664658fbca358850b71fc41c15dd553932acd9
alamir-1 <mohamedabdoumostfa@gmail.com>
2026-07-04 03:46:58 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'SEVEN1 WebView',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'SEVEN WebView',
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const WebViewScreen(),
     );
   }
@@ -29,50 +28,26 @@ class WebViewScreen extends StatefulWidget {
 }
 
 class _WebViewScreenState extends State<WebViewScreen> {
-  late final WebViewController _controller;
+  InAppWebViewController? _controller;
   bool _isLoading = true;
 
-static const String _siteUrl = 'https://alamir-1.github.io/SEVEN-27-3/';
+  static const String _siteUrl = 'https://alamir-1.github.io/SEVEN1/';
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageStarted: (String url) {
-            setState(() {
-              _isLoading = true;
-            });
-          },
-          onPageFinished: (String url) {
-            setState(() {
-              _isLoading = false;
-            });
-          },
-          onWebResourceError: (WebResourceError error) {
-            debugPrint('WebView Error: ${error.description}');
-          },
-        ),
-      );
-    _loadFreshPage();
+  Uri _freshUri() {
+    return Uri.parse(
+      '$_siteUrl?_t=${DateTime.now().millisecondsSinceEpoch}',
+    );
   }
 
-  Future<void> _loadFreshPage() async {
-    // امسح الكاش قبل كل تحميل عشان تضمن أحدث نسخة من الموقع دايماً
-    await _controller.clearCache();
-    await _controller.clearLocalStorage();
-    // إضافة رقم عشوائي في نهاية الرابط يجبر المتصفح يتجاهل أي نسخة محفوظة
-    final freshUrl =
-        '$_siteUrl?_t=${DateTime.now().millisecondsSinceEpoch}';
-    await _controller.loadRequest(Uri.parse(freshUrl));
+  Future<void> _reload() async {
+    await _controller?.loadUrl(
+      urlRequest: URLRequest(url: WebUri.uri(_freshUri())),
+    );
   }
 
   Future<bool> _onWillPop() async {
-    if (await _controller.canGoBack()) {
-      _controller.goBack();
+    if (_controller != null && await _controller!.canGoBack()) {
+      _controller!.goBack();
       return false;
     }
     return true;
@@ -93,15 +68,71 @@ static const String _siteUrl = 'https://alamir-1.github.io/SEVEN-27-3/';
         body: SafeArea(
           child: Stack(
             children: [
-              WebViewWidget(controller: _controller),
-              if (_isLoading)
-                const Center(
-                  child: CircularProgressIndicator(),
+              InAppWebView(
+                initialUrlRequest: URLRequest(url: WebUri.uri(_freshUri())),
+                initialSettings: InAppWebViewSettings(
+                  javaScriptEnabled: true,
+                  useOnDownloadStart: true,
+                  useShouldOverrideUrlLoading: true,
+                  mixedContentMode:
+                      MixedContentMode.MIXED_CONTENT_ALWAYS_ALLOW,
                 ),
+                onWebViewCreated: (controller) {
+                  _controller = controller;
+                },
+                onLoadStart: (controller, url) {
+                  setState(() => _isLoading = true);
+                },
+                onLoadStop: (controller, url) {
+                  setState(() => _isLoading = false);
+                },
+                onReceivedError: (controller, request, error) {
+                  debugPrint('WebView Error: ${error.description}');
+                },
+                // هنا بيتم التعامل مع أي محاولة تحميل ملف (PDF أو صورة)
+                onDownloadStartRequest: (controller, downloadStartRequest) async {
+                  final fileUrl = downloadStartRequest.url.toString();
+                  final uri = Uri.parse(fileUrl);
+                  // بيفتح الملف بمتصفح الجهاز (Chrome) اللي هيحمله وينزله
+                  // على مجلد التنزيلات (Downloads) بشكل طبيعي وتلقائي
+                  if (await canLaunchUrl(uri)) {
+                    await launchUrl(
+                      uri,
+                      mode: LaunchMode.externalApplication,
+                    );
+                  }
+                },
+                // لو الموقع فتح رابط ملف مباشرة (زي رابط PDF) بدل ما يبدأ تحميل
+                shouldOverrideUrlLoading: (controller, navigationAction) async {
+                  final url = navigationAction.request.url.toString();
+                  final lower = url.toLowerCase();
+                  if (lower.endsWith('.pdf') ||
+                      lower.endsWith('.jpg') ||
+                      lower.endsWith('.jpeg') ||
+                      lower.endsWith('.png')) {
+                    final uri = Uri.parse(url);
+                    if (await canLaunchUrl(uri)) {
+                      await launchUrl(
+                        uri,
+                        mode: LaunchMode.externalApplication,
+                      );
+                      return NavigationActionPolicy.CANCEL;
+                    }
+                  }
+                  return NavigationActionPolicy.ALLOW;
+                },
+              ),
+              if (_isLoading)
+                const Center(child: CircularProgressIndicator()),
             ],
           ),
         ),
-    ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _reload,
+          tooltip: 'تحديث الصفحة',
+          child: const Icon(Icons.refresh),
+        ),
+      ),
     );
   }
 }
